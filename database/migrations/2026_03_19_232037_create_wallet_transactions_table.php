223001e18fdec187d0d6554e30c0a1e21d2042b9
abd <alssadiabdalrahman5@gmail.com>
2026-03-20 02:36:02 +0300

    change

--- database/migrations/2026_03_19_232037_create_wallet_transactions_table.php
+++ database/migrations/2026_03_19_232037_create_wallet_transactions_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('wallet_transactions', function (Blueprint $table) {
             $table->id();
-            $table->id();
             $table->foreignId('user_id');
             $table->decimal('amount', 10, 2);
             $table->string('type'); // deposit / withdraw
